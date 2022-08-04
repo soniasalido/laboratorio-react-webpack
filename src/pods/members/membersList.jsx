3315695d11e941b6d392e576a10f560d1bd5d744
soniasalido <soniasalido@gmail.com>
2022-08-04 20:48:20 +0200

    Casi conseguido que todo el trabajo hecho en vite entre en webpack. No va fino con el query-params que funciona en Vite pero en webpack no va bien

--- src/pods/members/membersList.jsx
+++ src/pods/members/membersList.jsx
@@ -1,32 +1,26 @@
 import { useMemo, useState, useEffect } from "react";
 import React from "react";
-import { GetMembersMockDataByOrganization } from "./getMembersMockDataByOrganization.jsx";
-// import {useMembersApiByOrganization}  from "./useMembersApiByOrganization.js";
+// import { GetMembersMockDataByOrganization } from "./getMembersMockDataByOrganization.jsx";
+// import { useMembersApiByOrganization }  from "./useMembersApiByOrganization.js";
 import { MemberCardList } from "./memberCardList.jsx";
 
 
-
 export const MembersList = ({ organization }) => {
-    //const [members , setMembers ] = useState([])
-    // const gitHubMembersUrl =  `https://api.github.com/orgs/${organization}/members`;
-
-    // Usamos el hook useMemo para memorizar el valor de members. Si el padre cambia, pide otro tipo de organization,
-    // entonces se vuelve a hacer una nueva petición. En caso contrario, se usa el valor que ya tenía memorizado.
-    // En la renderización si está vacío members (no existe esa organización), entonces no se retorna nada.
-    const members = useMemo( () => GetMembersMockDataByOrganization( organization ), [ organization ]);
-    //const members = useMemo( () => useMembersApiByOrganization( organization ), [ organization ]);
+    const [members , setMembers ] = useState([])
+    const gitHubMembersUrl =  `https://api.github.com/orgs/${organization}/members`;
 
 
-    // useEffect(() => {
-    //     fetch(gitHubMembersUrl)
-    //         .then(response => response.json())
-    //         .then(data => {
-    //             setMembers(data);
-    //         }).catch(error => {
-    //             console.log(error);
-    //         }
-    //     );
-    // }, [organization]);
+    useEffect( () => {
+        fetch(gitHubMembersUrl)
+            .then(response => response.json())
+            .then(data => {
+                setMembers(data);
+            }).catch(error => {
+                console.log(error);
+            }
+        );
+    }, [organization]);
+
 
     return (
             <div className="container mt-5 animate__animated animate__fadeIn">
